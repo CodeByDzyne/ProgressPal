716a599fe7ff2f7339daf1622d31320749758c52
CodeByDzyne <wdenley@dzynesolutions.com>
2023-10-23 19:21:53 +1100

    Initial Commit

---
+++ ProgressPal/ProgressPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressPalApp.swift
+//  ProgressPal
+//
+//  Created by Wealthy Waz on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
